d139d7863864643fd0b8c45ead2619073ed405a0
Yody Andrea Urbano Molano <yody_urbano@soy.sena.edu.co>
2024-10-23 01:52:49 +0000

    Updated Funciones.js

--- Funciones.js
+++ Funciones.js
@@ -22,7 +22,7 @@ document.querySelector('#Formulario').addEventListener('submit', async (event) =
     console.log('Datos a enviar:', data);
 
     try {
-        const response = await fetch('http://179.32.229.104:10000/servicios', {
+        const response = await fetch('https://ripe2-apim.azure-api.net/servicios', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
