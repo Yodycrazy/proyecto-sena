02fe52b5defb2d8f74eae3ff169af28beafe94d2
YODY <yaurbanom@ut.edu.co>
2024-11-06 17:07:18 -0500

    actualizar

--- Funciones.js
+++ Funciones.js
@@ -22,7 +22,7 @@ document.querySelector('#Formulario').addEventListener('submit', async (event) =
     console.log('Datos a enviar:', data);
 
     try {
-        const response = await fetch('http://localhost:10000/servicios', {
+        const response = await fetch('http://142.44.247.98:3006/servicios', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
